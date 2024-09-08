446abea21e24b2b045098fe491f4a92c6d12f26d
Miguel Angel Gonzalez Norena <miguel.gn@globant.com>
2024-09-07 21:28:06 -0500

    added category and product entities

---
+++ src/main/java/com/onioncoders/grandmasfood/domain/entities/ProductEntity.java
@@ -0,0 +1,29 @@
+package com.onioncoders.grandmasfood.domain.entities;
+
+import java.util.UUID;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity (name = "product")
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Data
+public class ProductEntity {
+
+    @Id
+    private UUID id;
+    private String name;
+    private Integer price;
+    private Integer stock;
+
+    @JoinColumn(name = "categoryfk")
+    private CategoryEntity category;
+    
+}
--- src/main/java/com/onioncoders/grandmasfood/domain/repositories/OrderRepository.java
+++ src/main/java/com/onioncoders/grandmasfood/domain/repositories/OrderRepository.java
@@ -3,9 +3,9 @@ package com.onioncoders.grandmasfood.domain.repositories;
 import java.util.UUID;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.CrudRepository;
 
 import com.onioncoders.grandmasfood.domain.entities.OrderEntity;
 
-public interface OrderRepository extends JpaRepository<OrderEntity,UUID>{
+public interface OrderRepository extends JpaRepository<OrderEntity,UUID>{}
     
-}
